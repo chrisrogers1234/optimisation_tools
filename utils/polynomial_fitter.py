575e29c8691897d879c09e6da0f2a258867b1299
Chris Rogers <chris.rogers@stfc.ac.uk>
2025-08-14 10:22:04 +0100

    Add comments to polynomial fitter

--- utils/polynomial_fitter.py
+++ utils/polynomial_fitter.py
@@ -4,14 +4,50 @@ import numpy
 import scipy.optimize
 import matplotlib.pyplot as plt
 
+
+
 class PolynomialFitter(object):
+    """
+    Try to find polynomial s.t.
+
+    y = y_0 + M_2 x + M_3 x * x
+
+    where
+    * y and x are vectors of dimension N
+    * M_2 is a matrix of dimension N x N
+    * M_3 is a tensor of dimension N x N x N
+
+    In order to do the fitting, we use a fit vector like
+    v_0 = [m_0, m2_00 x_0, m2_01 x_1, ..., m3_000 x_0*x_0, m3_001 x_0*x_1, ...
+
+    v_i = [m_i, ..., m2_ij x_j,  ..., m3_ijk x_j x_k, ...]
+
+    The aim is to minimise the m values so that y is as close as possible to the
+    inputs for given x inputs.
+
+    In accelerator jargon, M_2 is the transfer matrix and y_0 is the closed orbit.
+    M_3 is a quadratic correction factor.
+
+
+    If self.is_linear is true, m3_ijk are all assumed to be 0
+    """
+
+
     def __init__(self, n_dimensions):
+        """
+        Initialise the fitter
+        """
         self._arr_d = 0
         self.test_matrix = None
         self.is_linear = True
         self.set_n_dimensions(n_dimensions)
 
     def set_n_dimensions(self, n_dimensions):
+        """
+        Set the dimension
+
+        Here we just set the length of v for convenience later on
+        """
         self.n_dimensions = n_dimensions
         self.vec_length = 1+self.n_dimensions
         if self.is_linear:
@@ -22,6 +58,9 @@ class PolynomialFitter(object):
 
 
     def quadratic(self, x_data, matrix):
+        """
+        Calculate the v vector assuming quadratic expansion
+        """
         y_data = []
         for x_item in x_data:
             y_item = [0.0 for i in range(self.n_dimensions)]
@@ -36,6 +75,9 @@ class PolynomialFitter(object):
         return y_data
 
     def linear(self, x_data, matrix):
+        """
+        Calculate the v vector assuming linear expansion
+        """
         y_data = []
         for x_item in x_data:
             y_item = [0.0 for i in range(self.n_dimensions)]
@@ -57,32 +99,35 @@ class PolynomialFitter(object):
         return x_data, y_data
 
     def linear_fit_function(self, x_data, *args):
+        """
+        Fit using linear expansion
+        """
         test_matrix = [args]
         x_data = x_data.tolist()
         y_item = self.linear(x_data, test_matrix)
         y_item = numpy.array(y_item)
         y_item_0 = y_item[::, 0:1].flatten()
-
-        #print("Fit function on", len(x_data), "points\n", x_data)
-        #print("Using test matrix\n", numpy.array(test_matrix))
-        #print("Yields\n", y_item_0)
-
         return y_item_0
 
     def quad_fit_function(self, x_data, *args):
+        """
+        Fit using quadratic expansion
+        """
         test_matrix = [args]
         x_data = x_data.tolist()
         y_item = self.quadratic(x_data, test_matrix)
         y_item = numpy.array(y_item)
         y_item_0 = y_item[::, 0:1].flatten()
-
-        #print("Fit function on", len(x_data), "points\n", x_data)
-        #print("Using test matrix\n", numpy.array(test_matrix))
-        #print("Yields\n", y_item_0)
-
         return y_item_0
 
     def fit_transfer_map(self, x_data, y_data):
+        """
+        Do the fit.
+
+        I use scipy.optimize.curve_fit. In principle it is a linear problem,
+        so one could use linear least squares, but this way we can get an
+        improvement if there is noise and the problem is overconstrained.
+        """
         x_data = numpy.array(x_data)
         y_data = numpy.array(y_data)
         max_dim = self.n_dimensions
@@ -107,6 +152,9 @@ class PolynomialFitter(object):
               separator=', '))
 
 def main():
+    """
+    This runs a simple test
+    """
     fitter = PolynomialFitter(4)
     x_data, y_data = fitter.test_data()
     fit = fitter.fit_transfer_map(x_data, y_data)
